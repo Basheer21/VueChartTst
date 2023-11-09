3389af44c17b7f8254e7ce370de1b1d4daa28279
Basheer Raad <basheerraad5@gmail.com>
2023-11-09 12:51:55 +0300

    Your commit message

---
+++ resources/js/Components/ChartComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h3>hello world!</h3>
+        <canvas id="chartCanvas"></canvas>
+    </div>
+</template>
+
+<script setup>
+// import { ref, onMounted } from 'vue'; // this line is added for vue
+
+import Chart from "chart.js/auto";
+import { onMounted } from "vue";
+
+//we only added the mounted part
+
+onMounted(() => {
+    const ctx = document.getElementById("chartCanvas");
+
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [
+                {
+                    label: "# of Votes",
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+});
+</script>
---
+++ resources/js/Components/ChartComponent2.vue
@@ -0,0 +1,44 @@
+<template>
+    <Bar :chart-data="chartData" />
+</template>
+
+<script>
+const chartData = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    datasets: [
+        {
+            label: "Dataset 1",
+            data: [2, 5, 4, 8, 10, 4, 7],
+            backgroundColor: "#DB4C77",
+        },
+    ],
+};
+import { Bar } from "vue-chartjs";
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+} from "chart.js";
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+);
+export default {
+    components: {
+        Bar,
+    },
+    data() {
+        return {
+            chartData: chartData,
+        };
+    },
+};
+</script>
